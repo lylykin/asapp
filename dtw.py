7d981d168455a7ba4dc8acc493e4a979fbb80e7b
lylykin <clelie.jalbert@gmail.com>
2025-04-23 17:23:21 +0200

    feat : worked on debugging _AccCostMatrix (not finished)

--- dtw.py
+++ dtw.py
@@ -13,7 +13,9 @@ class Dtw :
         self.refserie = refserie   
         self.compserie = compserie
 
-        
+    def __str__(self):
+        return f'{self.acm}'
+    
     def CostMatrix(self) : 
         """
         return a matrix of distance : we'll have the distances of every points of the two series
@@ -29,16 +31,19 @@ class Dtw :
 
         self.cm = mat
     
-    def AccCostMatrix(self, i, j): 
+    def AccCostMatrix(self,): 
         """
         returns the accumulation cost matrix.
-        """        
+        """ 
+        i = len(self.cm)
+        j = len(self.cm[0])
+        
         #matrice initialization to make my life easier
         init_mat = [[_ for _ in range(j)]
                     for k in range(i)]
                
         #i'm still not sure if those matrix should be attributes or not, but we can stock them in one objet if done this way ig
-        self.acm = self._AccCostMatrix(self,len(self.cm), len(self.cm[0]), init_mat)  
+        self.acm = self._AccCostMatrix(i-1, j-1, init_mat)  
 
     def _AccCostMatrix(self, i, j, c_mat) : #might have a high cost, we'll see if that's an issue or not
         """
@@ -48,14 +53,17 @@ class Dtw :
         if i ==0 or j==0 : 
             return math.inf 
         
-        #the distance here iis the same as the euclidian distance
+        #the distance here iis tshe same as the euclidian distance
         if i ==1 and j == 1:
             return self.cm[i][j]
         
         #the disnace here is the euclidian distance with the smallest distance from its neighbours in the C matrix
         else :
-            c_mat[i][j] = self.cm[i][j] + min(c_mat[i-1][j], c_mat[i][j-1], c_mat[i-1][j-1])
+            c_mat[i][j] = self.cm[i][j] + min(self._AccCostMatrix(i-1, j, c_mat), self._AccCostMatrix(i, j-1, c_mat), self._AccCostMatrix(i-1, j-1, c_mat))
         
         
         
-    
+d = Dtw([1,3,4],[1,2,4])
+d.CostMatrix()
+d.AccCostMatrix()
+print(d)
