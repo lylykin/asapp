9aedc4d224f8caa068d02ebf1fb4f159177c5bae
LePouceBleu <rochedorian26@gmail.com>
2025-05-05 20:15:17 +0200

    Début de la rédaction du front end avec récupération des points tracés
    - dico pour stocker les traits sous forme de liste de paires de points
    - on peut tracer des points affichés sur un canvas avec le clic gauche
    - ajout de la bibliotheque customtkinter dans les dépendances

---
+++ appWindow.py
@@ -0,0 +1,56 @@
+import customtkinter as ctk
+import time
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Asapp")
+        self.geometry("600x500")
+        self.widget_placement()
+        self.widget_interact()
+
+        self.n_strokes = 0 # Nombre de traits dessinés depuis l'init
+        self.strokes = {} # Dico stockant les traits tracés sous forme de liste de paires de points associés à un id (1 à infini)
+        
+    def widget_placement(self):
+        '''
+        Définis les différents widget à placer dans la fenêtre
+        '''
+        self.main_canvas = ctk.CTkCanvas(width=800, height=800, bg="white")
+        self.main_canvas.grid(row=0,column=0)
+
+    def widget_interact(self):
+        '''
+        Définis les liaisons entre les widget et les interactions de l'utilisateur
+        '''
+        self.main_canvas.bind("<B1-Motion>", self.canvas_draw_stroke) # Détection de mouvement avec clic gauche de la souris sur le canvas
+        self.main_canvas.bind("<ButtonPress-1>", self.canvas_new_stroke)
+        self.main_canvas.bind("<ButtonRelease-1>", self.canvas_end_stroke) # debug pour l'instant
+
+    def canvas_new_stroke(self, event):
+        '''
+        Mesure un nouveau clic gauche de l'utilisateur et ajoute 1 au compteur (initialement nul) des traits tracés
+        '''
+        self.n_strokes += 1
+        self.strokes[self.n_strokes] = [] # Définis la liste à remplir pour un nouveau trait
+    
+    def canvas_end_stroke(self, event):
+        '''
+        Permet d'observer le résultat du stockage des points du tracé
+        ! temporaire !
+        '''
+        print(self.strokes[self.n_strokes])
+
+    def canvas_draw_stroke(self, event):
+        x = event.x
+        y = event.y
+        cursor_pos = (x, y) # Récupère les coordonnées de la souris (relativement au 0,0 du canvas)
+        self.strokes[self.n_strokes].append(cursor_pos) # Ajoute les coorodnnées à la liste de points courante
+        self.main_canvas.create_oval(x-1, y-1, x+1, y+1, fill='black', width=4) # Affichage du point
+        print(f"pos : {cursor_pos}, n_strokes : {self.n_strokes}") # DEBUG
+        #time.sleep(100) # Essai de mettre du délai pour éviter d'avoir trop de données : CAUSE UN BUG, PAS CAPTE PK
+
+
+asapp = App()
+asapp.mainloop()
+
--- readme.md
+++ readme.md
@@ -6,6 +6,7 @@
 
 
 - Python 3.8 or higher
+- customtkinter 5.2.2 or higher
 
 ```py
 $ pip install svg.path
