834996a1c03805b5f7bc6e46f0aa5a35ae9cfdb1
LePouceBleu <rochedorian26@gmail.com>
2025-05-13 17:47:05 +0200

    add : nouveau thème personalisé pour le front end edit : ajouts de nouveaux boutton pour quitter et réinitialiser le canvas, amélioration de l'apparence

--- appWindow.py
+++ appWindow.py
@@ -1,31 +1,45 @@
 import customtkinter as ctk
 from PIL import Image
+from colour import Color
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Asapp")
         self.geometry("600x600")
-        self.widget_placement()
-        self.widget_interact()
+        ctk.set_default_color_theme("assets/asapp_theme.json") # Mets un thème custom pour les widget par défaut
 
-        self.main_color = "dark_orange"
-        self.second_color = "orange"
+        self.main_color = "#D76400"
+        self.second_color = "#A0522D" # Définitions de couleurs à retirer si on utilise le theme.json
         self.n_strokes = 0 # Nombre de traits dessinés depuis l'init
         self.strokes = {} # Dico stockant les traits tracés sous forme de liste de paires de points associés à un id (1 à infini)
         
+        self.widget_placement()
+        self.widget_interact()
+
     def widget_placement(self):
         '''
         Définis les différents widget à placer dans la fenêtre
         '''
-        self.main_canvas = ctk.CTkCanvas(width=800, height=800, bg="white")
+        # Définitions des Widget
+        self.canvas_frame = ctk.CTkFrame(self) # Stocke le canvas
+        self.main_canvas = ctk.CTkCanvas(self.canvas_frame, bg="white", borderwidth=3, cursor="tcross") 
         logo = ctk.CTkImage(light_image=Image.open("assets/logo.png"), size=(50, 50))
         # ctk.CTkImage(light_image=Image.open("<path to light mode image>"), dark_image=Image.open("<path to dark mode image>"), size=(30, 30))
         self.logo_label = ctk.CTkLabel(self, image=logo, text="App Logo")  # display image with a CTkLabel
-        self.compare_button = ctk.CTkButton(self, corner_radius=2, bg_color=self.second_color, )
+        self.compare_button = ctk.CTkButton(self.canvas_frame, border_width=3, corner_radius=5, anchor="center", text="Comparer le caractère")
+        self.clear_button = ctk.CTkButton(self.canvas_frame, border_width=3, corner_radius=5, anchor="center", text="Effacer")
+        self.exit_button = ctk.CTkButton(self, border_width=3, corner_radius=5, anchor="center", text="Quitter")
 
+        # Position des widgets dans l'app
         self.logo_label.grid(row=0,column=0)
-        self.main_canvas.grid(row=1,column=1)
+        self.canvas_frame.grid(row=1,column=1, sticky="nsew")
+        self.exit_button.grid(row=2,column=0, sticky="s")
+
+        # Position des widget dans la canvas_frame
+        self.main_canvas.grid(row=0,column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        self.compare_button.grid(row=1, column = 0, columnspan=2, padx=15, pady=10, sticky="ew") 
+        self.clear_button.grid(row=2, column=1, padx=5, sticky="e")
 
     def widget_interact(self):
         '''
@@ -34,10 +48,13 @@ class App(ctk.CTk):
         self.main_canvas.bind("<B1-Motion>", self.canvas_draw_stroke) # Détection de mouvement avec clic gauche de la souris sur le canvas
         self.main_canvas.bind("<ButtonPress-1>", self.canvas_new_stroke)
         self.main_canvas.bind("<ButtonRelease-1>", self.canvas_end_stroke) # debug pour l'instant
+        self.exit_button.bind("<ButtonPress-1>", self.end_app)
+        self.clear_button.bind("<ButtonPress-1>", self.clear_canvas)
 
     def canvas_new_stroke(self, event):
         '''
-        Mesure un nouveau clic gauche de l'utilisateur et ajoute 1 au compteur (initialement nul) des traits tracés
+        Mesure un nouveau clic gauche de l'utilisateur sur le canvas
+        et ajoute 1 au compteur (initialement nul) des traits tracés.
         '''
         self.n_strokes += 1
         self.strokes[self.n_strokes] = [] # Définis la liste à remplir pour un nouveau trait
@@ -50,13 +67,28 @@ class App(ctk.CTk):
         print(self.strokes[self.n_strokes])
 
     def canvas_draw_stroke(self, event):
+        '''
+        Appelé quand l'utilisateur maintiens le clic sur le canvas.
+        Place un point noir aux coordonnées de la souris, puis ajoute les coordonées au dictionnaire des tracés.
+        '''
         x = event.x
         y = event.y
         cursor_pos = (x, y) # Récupère les coordonnées de la souris (relativement au 0,0 du canvas)
         self.strokes[self.n_strokes].append(cursor_pos) # Ajoute les coordonnées à la liste de points courante
-        self.main_canvas.create_oval(x-1, y-1, x+1, y+1, fill='black', width=4) # Affichage du point
-        print(f"pos : {cursor_pos}, n_strokes : {self.n_strokes}") # DEBUG
+        self.main_canvas.create_oval(x-1, y-1, x+1, y+1, tags="user_stroke_dot", fill='black', width=4) # Affichage du point
 
+    def end_app(self, event):
+        '''
+        Termine l'application
+        '''
+        self.quit()
+    
+    def clear_canvas(self, event):
+        '''
+        Supprime tous les points tracés par l'utilisateur (tag : user_stroke_dot) et vide le dictionnaire contenant les tracés.
+        '''
+        self.main_canvas.delete("user_stroke_dot")
+        self.strokes = {} 
 
 asapp = App()
 asapp.mainloop()
