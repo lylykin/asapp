4f18202dd86c67356eb3d0651cba17384c1501c9
DoubleD3269 <didac.espin@gmail.com>
2025-05-13 17:56:02 +0200

    feat : started Controller class

--- controller.py
+++ controller.py
@@ -1,6 +1,10 @@
-from kanji import Kanji
+from kanji import Kanji, KanjiDB
 from dtw import Dtw
 from dictionary import Dictionnary
 from appWindow import App
 from svg_path import Path
 from identifier import *
+
+class Controller :
+    app : App
+    db : KanjiDB
