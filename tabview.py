343b76962483cd6ddf1bffbd1440debde414f1b4
LePouceBleu <rochedorian26@gmail.com>
2025-05-25 14:48:19 +0200

    add : définitions des nouveaux widget pour le dictionnaire TODO : gérer le placement dysfonctionnel de ces derniers

--- tabview.py
+++ tabview.py
@@ -45,20 +45,45 @@ class TabView(ctk.CTkTabview):
     def widget_dictionary_placement(self, master):
         '''
         Définis les différents widget à placer dans la fenêtre de la tab dictionnaire
-        /!\ Penser à bien donner des noms différents aux widgets de la tab dictionnaire
+        /!\\Penser à bien donner des noms différents aux widgets de la tab dictionnaire
         '''
         # Définitions des Widget
-        self.canvas_frame2 = ctk.CTkFrame(master) # Stocke le canvas
-        self.compare_button2 = ctk.CTkButton(self.canvas_frame2, border_width=3, corner_radius=5, anchor="center", text="YAY LA TAB 2 GENRE")
+        self.menu_button_dictionary = ctk.CTkButton(master, text="Bon le menu là", anchor="center")
+        self.desc_frame = ctk.CTkFrame(master, width=400, height=400, fg_color="red")
+        self.kanji_name_label = ctk.CTkLabel(self.desc_frame, text="", font=(ctk.CTkFont(family="comic sans ms", underline=True)))
+        self.desc_label = ctk.CTkLabel(self.desc_frame, text="")
+        self.kanji_frame_dictionary = ctk.CTkFrame(master, width=400, height=400, fg_color="white")
+        self.kanji_display_dictionary = ctk.CTkLabel(self.kanji_frame_dictionary, text = "", text_color="black")
 
-        # Position des frame dans la tab
-        self.canvas_frame2.grid(row=0,column=0, sticky="nsew")
+        # Position des frame et widgets dans la tab
+        self.menu_button_dictionary.grid(row=2,column=0) #, sticky="sw"
+        self.kanji_display_dictionary.grid(row=0, column=0) #, sticky="nw"
+        self.desc_frame.grid(row=0, column=1, sticky="e") #, rowspan=2
+        self.kanji_frame_dictionary.grid(row=1, column=0) #, sticky="ns"
 
-        # Position des widget dans la canvas_frame
-        self.compare_button2.grid(row=1, column = 0, columnspan=2, padx=15, pady=5, sticky="ew") 
+        # Position des widget dans la desc_frame
+        self.kanji_name_label.grid(row=0, column = 0, columnspan=2, padx=15, pady=5, sticky="ew") 
+        self.desc_frame.grid(row=1, column = 0, columnspan=2, padx=15, pady=5, sticky="ew") 
+
+        # Position des widget dans la kanji_frame_dictionary
+        self.kanji_display_dictionary.grid(row=0, column=0, sticky="nsew")
 
         # Définit la répartition globale de taille des frames pour les colonnes et lignes
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=2)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_propagate(False)
+        #self.grid_rowconfigure(0, weight=1)
+        #self.grid_columnconfigure(0, weight=1)
+        #self.grid_columnconfigure(1, weight=2)
+        #self.grid_propagate(False)
+
+        #DEBUG
+        self.display_kanji_dictionary(':D')
+    
+    def display_kanji_dictionary(self, kanji):
+        '''
+        Permet d'afficher et d'ajouter les descriptions noms et apparence des kanjis selon le paramètre d'entrée
+        EN DEVELOPPEMENT
+        '''
+        self.kanji_display_dictionary.configure(text=kanji)
+        kanji_desc = "Yosh la description" # A terme : kanji.desc
+        kanji_name = "Le nom de fou"
+        self.desc_label.configure(text=kanji_desc)
+        self.kanji_name_label.configure(text=kanji_name)
