f4a48ad0be567a27026eee5a9d9af52476c8edb0
cyp <supercyp971@gmail.com>
2025-06-02 17:07:36 +0200

    feat: add fast math function

---
+++ fast_math.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+# calcul de la racine carrée inverse 
+# https://ajcr.net/fast-inverse-square-root-python/ 
+def numpy_isqrt(number):
+    threehalfs = 1.5
+    x2 = number * 0.5
+    y = np.float32(number)
+    
+    i = y.view(np.int32)
+    i = np.int32(0x5f3759df) - np.int32(i >> 1)
+    y = i.view(np.float32)
+    
+    y = y * (threehalfs - (x2 * y * y))
+    return float(y)
+
+def fast_atan2(y, x):
+    """
+    Fast approximation of atan2 using numpy.
+    """
+    return np.arctan2(y, x)
